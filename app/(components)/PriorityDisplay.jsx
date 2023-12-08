fcc6bd251f1d5d38882ec3b28ead24355fdc8e2d
Ilhamroe <iramadani2003@gmail.com>
2023-12-08 23:22:56 +0700

    Schedule-App

---
+++ app/(components)/PriorityDisplay.jsx
@@ -0,0 +1,39 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const PriorityDisplay = ({ priority }) => {
+  return (
+    <div className="flex justify-start align-baseline">
+      <FontAwesomeIcon
+        icon={faStar}
+        className={` pr-1 ${
+          priority > 0 ? " text-red-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={` pr-1 ${
+          priority > 1 ? " text-red-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={`  pr-1 ${
+          priority > 2 ? " text-red-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={` pr-1 ${
+          priority > 3 ? " text-red-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={` ${priority > 4 ? " text-red-400" : " text-slate-400"}`}
+      />
+    </div>
+  );
+};
+
+export default PriorityDisplay;
